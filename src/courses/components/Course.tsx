d9846ac144d8f0c77941531a421dfc0bfd7e661a
inthhh <glass005533@gmail.com>
2025-05-28 11:06:20 +0900

    style: 코스 컴포넌트 스타일 수정

--- src/courses/components/Course.tsx
+++ src/courses/components/Course.tsx
@@ -8,26 +8,37 @@ const Course = (nowCourse: Interfaces.Course) => {
   );
   const [course, setCourse] = useState<Interfaces.Course | null>(nowCourse);
   return (
-    <>
-      <div>
-        <div>
-          {course?.userName} 님의 {course?.title} 코스
+    <div className={style.courseBlock}>
+      <div className={style.header}>
+        <div className={style.title}>
+          "{course?.userName}" 님의{" "}
+          <span style={{ color: "var(--color-main)" }}>{course?.title}</span>{" "}
+          코스
         </div>
-        <div>
+        <div className={style.like}>
           <div>{course?.courseLike}</div>
-          <div>{course?.like ? true : false}</div>
+          <div>{course?.like}</div>
         </div>
       </div>
       <div className={style.coursePlaceList}>
-        {course?.placeList.map((place) => (
-          <div key={place.placeId}>
-            <img src={place.imgUrl} alt={place.placeName} />
-            <div>{place.placeName}</div>
-            <div>{place.address}</div>
+        {course?.placeList.map((place, idx) => (
+          <div key={place.placeId} className={style.placeBlock}>
+            <img
+              src={place.imgUrl}
+              alt={place.placeName}
+              className={style.placeImg}
+            />
+            <div className={style.placeInfo}>
+              <div className={style.idx}>{idx}</div>
+              <div>{place.placeName}</div>
+              <div className={style.address}>
+                {place.address.split(" ").slice(0, 2).join(" ")}
+              </div>
+            </div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
