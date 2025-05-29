d7f26bda923800c02d9581baf5ea4e30d3aad4e5
heedong12 <dlgmldus3289@naver.com>
2025-05-29 09:38:13 +0900

    style: 회원정보 페이지 리팩토링 및 완료

--- src/signup/Signup.tsx
+++ src/signup/Signup.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Signup.module.css";
+import { ArrowRight } from "lucide-react";
 
 const Gender = {
   남: 0,
@@ -27,66 +28,97 @@ interface UserInfoType {
   gender: GenderValue | null;
 }
 
-const Signup = () => {
-  const [userInfo, setUserInfo] = useState<UserInfoType>({
-    // todo : 받아오는 userName으로 넣을 예정
-    userName: "이희연",
-    age: null,
-    gender: null,
-  });
+interface ButtonGroupProps<T> {
+  label: string;
+  options: Record<string, T>;
+  selected: T | null;
+  onSelect: (value: T) => void;
+}
 
+const ButtonGroup = <T,>({
+  label,
+  options,
+  selected,
+  onSelect,
+}: ButtonGroupProps<T>) => {
   return (
-    <div className={styles.signup_wrapper}>
-      <div className={styles.signup_title}>하루걸음에 오신 것을 환영해요:)</div>
-      <label className={styles.element_label} htmlFor="name">
-        사용하실 닉네임을 입력해주세요.
-      </label>
-      <input
-        id="name"
-        type="text"
-        placeholder="ex - 하루걸음"
-        value={userInfo.userName}
-        onChange={(e) =>
-          setUserInfo((prev) => ({
-            ...prev,
-            userName: e.target.value,
-          }))
-        }
-        className={`${styles.element} ${styles.input}`}
-      />
-      <div className={styles.element_label}>성별</div>
+    <>
+      <div className={styles.element_label}>{label}</div>
       <div className={styles.buttons}>
-        {Object.keys(Gender).map((key) => (
+        {Object.entries(options).map(([key, value]) => (
           <div
             key={key}
-            className={`${styles.element} ${styles.button} ${userInfo.gender === Gender[key as GenderKey] ? styles.selected_btn : ""}`}
-            onClick={() =>
-              setUserInfo((prev) => ({
-                ...prev,
-                gender: Gender[key as GenderKey],
-              }))
-            }
+            className={`${styles.element} ${styles.button} ${
+              selected === value ? styles.selected_btn : ""
+            }`}
+            onClick={() => onSelect(value)}
           >
             {key}
           </div>
         ))}
       </div>
-      <div className={styles.element_label}>연령대</div>
-      <div className={styles.buttons}>
-        {Object.keys(AgeGroup).map((key) => (
-          <div
-            key={key}
-            className={`${styles.element} ${styles.button} ${userInfo.age === AgeGroup[key as AgeGroupKey] ? styles.selected_btn : ""}`}
-            onClick={() =>
-              setUserInfo((prev) => ({
-                ...prev,
-                age: AgeGroup[key as AgeGroupKey],
-              }))
-            }
-          >
-            {key}
-          </div>
-        ))}
+    </>
+  );
+};
+
+const Signup = () => {
+  const [userInfo, setUserInfo] = useState<UserInfoType>({
+    // todo: kakao에서 받아온 값으로 변경
+    userName: "이희연",
+    age: null,
+    gender: null,
+  });
+
+  const handleChange = (key: keyof UserInfoType, value: any) => {
+    setUserInfo((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  const isFinished =
+    userInfo.userName && userInfo.age !== null && userInfo.gender !== null;
+  return (
+    <div className={styles.container}>
+      <div className={styles.signup_wrapper}>
+        <div className={styles.signup_title}>
+          하루걸음에 오신 것을 환영해요:)
+        </div>
+
+        <label className={styles.element_label} htmlFor="name">
+          사용하실 닉네임을 입력해주세요.
+        </label>
+        <input
+          id="name"
+          type="text"
+          placeholder="ex - 하루걸음"
+          value={userInfo.userName}
+          onChange={(e) => handleChange("userName", e.target.value)}
+          className={`${styles.element} ${styles.input}`}
+        />
+
+        <ButtonGroup
+          label="성별"
+          options={Gender}
+          selected={userInfo.gender}
+          onSelect={(value) => handleChange("gender", value)}
+        />
+
+        <ButtonGroup
+          label="연령대"
+          options={AgeGroup}
+          selected={userInfo.age}
+          onSelect={(value) => handleChange("age", value)}
+        />
+      </div>
+
+      <div className={styles.btn_wrapper}>
+        <div
+          className={`${styles.next_btn} ${isFinished ? styles.active : ""}`}
+        >
+          다음
+          <ArrowRight size={20} color="#FFF" />
+        </div>
       </div>
     </div>
   );
