d8b40d68850031c7361ba13784dd5d5e9aade470
heedong12 <dlgmldus3289@naver.com>
2025-05-29 14:08:16 +0900

    style: 취향 입력 페이지 완료

--- src/signup/pages/UserLike.tsx
+++ src/signup/pages/UserLike.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Signup.module.css";
 import { data } from "../data";
-import { Check } from "lucide-react";
+import { ArrowRight, Check } from "lucide-react";
 
 interface Tag {
   tag: string;
   tagId: string;
+  checked: boolean;
+  handleChange: () => void;
 }
 
 interface SelectedTagList {
@@ -13,15 +15,16 @@ interface SelectedTagList {
   tagList: string[];
 }
 
-interface Category {
-  categoryId: string;
-  categoryName: string;
-}
-
-const SelectTag = ({ tag, tagId }: Tag) => {
+const SelectTag = ({ tag, tagId, checked, handleChange }: Tag) => {
   return (
-    <div className={styles.tag_wrapper}>
-      <input type="checkbox" className={styles.checkbox} id={tagId} />
+    <div className={styles.tag_wrapper} onClick={handleChange}>
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        id={tagId}
+        checked={checked}
+        readOnly
+      />
       <label htmlFor={tagId}>{tag}</label>
       <Check
         color="#FFF"
@@ -40,8 +43,8 @@ const UserLike = () => {
     categoryName: category.categoryName,
   }));
   const tagList = data.categoryList.map((category) => category.tagList);
-  const [selected, setSelected] = React.useState<number[]>([]);
-  const [selectedList, setSelectedList] = React.useState<SelectedTagList[]>([]);
+  const [selected, setSelected] = useState<number[]>([]);
+  const [selectedTagList, setSelectedTagList] = useState<SelectedTagList[]>([]);
 
   const handleClickCategory = (index: number) => {
     setSelected((prev) => {
@@ -49,18 +52,62 @@ const UserLike = () => {
         return prev.filter((item) => item !== index);
       } else {
         if (prev.length >= 3) {
-          alert("최대 3개까지만 선택이 가능합니다.");
+          alert("카테고리는 최대 3개까지만 선택이 가능합니다.");
           return prev;
         } else return [...prev, index];
       }
     });
   };
 
+  const handleClickTag = (categoryId: string, tagId: string) => {
+    setSelectedTagList((prev) => {
+      const existing = prev.find((item) => item.categoryId === categoryId);
+
+      if (existing) {
+        const updatedTagList = existing.tagList.includes(tagId)
+          ? existing.tagList.filter((tag) => tag !== tagId)
+          : existing.tagList.length < 5
+            ? [...existing.tagList, tagId]
+            : (alert("태그는 최대 5개까지만 선택할 수 있습니다."),
+              existing.tagList);
+
+        if (updatedTagList.length === 0) {
+          return prev.filter((item) => item.categoryId !== categoryId);
+        }
+
+        return prev.map((item) =>
+          item.categoryId === categoryId
+            ? { ...item, tagList: updatedTagList }
+            : item,
+        );
+      } else {
+        return [...prev, { categoryId, tagList: [tagId] }];
+      }
+    });
+  };
+
+  const isFinished = () => {
+    if (selected.length < 1 || selected.length > 3) return false;
+    if (selectedTagList.length < 1 || selectedTagList.length > 3) return false;
+    for (let item of selectedTagList) {
+      if (item.tagList.length < 1 || item.tagList.length > 5) return false;
+    }
+    return true;
+  };
+
+  const handleClickCompleteBtn = () => {
+    console.log(selectedTagList);
+    if (!isFinished()) {
+      alert("카테고리와 태그를 올바르게 선택해주세요.");
+      return;
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.signup_wrapper}>
         <div className={styles.warning}>
-          카테고리는 1개 ~ 3개, 태그는 1개 ~ 5개 선택해주세요!
+          카테고리는 1개 ~ 3개, 카테고리 당 태그는 1개 ~ 5개 선택해주세요!
         </div>
         <div className={styles.element_label}>
           어떤 장소를 찾아보러 오셨나요?
@@ -93,10 +140,29 @@ const UserLike = () => {
                   key={tag.tagId}
                   tag={tag.keyword}
                   tagId={tag.tagId}
+                  checked={
+                    selectedTagList
+                      .find(
+                        (item) =>
+                          item.categoryId === categories[index].categoryId,
+                      )
+                      ?.tagList.includes(tag.tagId) ?? false
+                  }
+                  handleChange={() => {
+                    handleClickTag(categories[index].categoryId, tag.tagId);
+                  }}
                 />
               ))}
             </div>
           ))}
+        </div>
+      </div>
+      <div className={styles.btn_wrapper}>
+        <div
+          className={`${styles.next_btn} ${isFinished() ? styles.active : ""}`}
+          onClick={handleClickCompleteBtn}
+        >
+          완료 !
         </div>
       </div>
     </div>
