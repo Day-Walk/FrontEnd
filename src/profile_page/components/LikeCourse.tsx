f496f5fd216af7672d8e8a1e7efdd68cf1553c98
inthhh <glass005533@gmail.com>
2025-06-09 15:38:57 +0900

    feat: Profile - 내 코스/ 리뷰/ 찜한 코스/ 찜한 장소 api 연동

--- src/profile_page/components/LikeCourse.tsx
+++ src/profile_page/components/LikeCourse.tsx
@@ -4,23 +4,52 @@ import Course from "./LikeCourseComp";
 import * as Interfaces from "../interfaces/Interfaces";
 import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
+import { useRecoilValue } from "recoil";
+import { userId } from "../../recoil/userInfo";
+import { api } from "../../utils/api";
 
 const LikeCourse = () => {
   const [coursePagesData, setCoursePagesData] =
-    useState<Interfaces.CourseListResponse>(Interfaces.dummyCourseList);
+    useState<Interfaces.CourseListResponse>();
 
   const [loading, setLoading] = useState<boolean>(true);
   const [nowPage, setNowPage] = useState<number>(1);
-  const [coursePage, setCoursePage] = useState<Interfaces.CoursePage>(
-    coursePagesData.courseList[nowPage - 1],
-  );
+  const [coursePage, setCoursePage] = useState<Interfaces.CoursePage>();
+  // coursePagesData.courseList[nowPage - 1],
+
+  const userIdState = useRecoilValue(userId);
+  const token = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      console.log("userIdState", userIdState);
+      try {
+        const response = await api.get<Interfaces.CourseListResponse>(
+          `/course-like/user?userId=${userIdState}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        setCoursePagesData(response.data);
+        setCoursePage(response.data.courseList[nowPage - 1]);
+        setLoading(false);
+        console.log("Fetched courses:", response.data);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+        setLoading(false);
+      }
+    };
+    fetchCourses();
+  }, []);
 
   const handleChangePage = (
     event: React.ChangeEvent<unknown>,
     value: number,
   ) => {
     setNowPage(value);
-    setCoursePage(coursePagesData.courseList[value - 1]);
+    setCoursePage(coursePagesData?.courseList[value - 1]);
   };
 
   useEffect(() => {
@@ -30,26 +59,34 @@ const LikeCourse = () => {
   return (
     <div className={style.courseWrapper}>
       <div className={style.subTitle}>찜한 코스</div>
-      <div>
-        {coursePage &&
-          coursePage.page.map((c, i) => (
-            <div
-              className={i === 0 ? undefined : style.courseList}
-              key={c.courseId}
-            >
-              <Course {...c} />
-            </div>
-          ))}
-      </div>
-      <div className={style.paginationWrapper}>
-        <Stack spacing={2}>
-          <Pagination
-            count={coursePagesData.courseList.length}
-            page={nowPage}
-            onChange={handleChangePage}
-          />
-        </Stack>
-      </div>
+      {coursePage ? (
+        <>
+          <div>
+            {coursePage &&
+              coursePage.page.map((c, i) => (
+                <div
+                  className={i === 0 ? undefined : style.courseList}
+                  key={c.courseId}
+                >
+                  <Course {...c} />
+                </div>
+              ))}
+          </div>
+          <div className={style.paginationWrapper}>
+            <Stack spacing={2}>
+              <Pagination
+                count={coursePagesData?.courseList.length}
+                page={nowPage}
+                onChange={handleChangePage}
+              />
+            </Stack>
+          </div>
+        </>
+      ) : (
+        <div style={{ margin: "30px 10px", color: "#aaa" }}>
+          찜한 코스가 아직 없어요. 😢
+        </div>
+      )}
     </div>
   );
 };
--- src/profile_page/components/MyCourse.tsx
+++ src/profile_page/components/MyCourse.tsx
@@ -1,26 +1,54 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import style from "../Profile.module.css";
 import Course from "./MyCourseComp";
 import * as Interfaces from "../interfaces/Interfaces";
 import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
+import { api } from "../../utils/api";
+import { userId } from "../../recoil/userInfo";
+import { useRecoilValue } from "recoil";
 
 const MyCourse = () => {
   const [coursePagesData, setCoursePagesData] =
-    useState<Interfaces.CourseListResponse>(Interfaces.dummyCourseList);
+    useState<Interfaces.CourseListResponse>();
 
   const [loading, setLoading] = useState<boolean>(true);
   const [nowPage, setNowPage] = useState<number>(1);
-  const [coursePage, setCoursePage] = useState<Interfaces.CoursePage>(
-    coursePagesData.courseList[nowPage - 1],
-  );
+  const [coursePage, setCoursePage] = useState<Interfaces.CoursePage>();
+  // coursePagesData.courseList[nowPage - 1],
+  const userIdState = useRecoilValue(userId);
+  const token = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      console.log("userIdState", userIdState);
+      try {
+        const response = await api.get<Interfaces.CourseListResponse>(
+          `/course/all/user?userId=${userIdState}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        setCoursePagesData(response.data);
+        setCoursePage(response.data.courseList[nowPage - 1]);
+        setLoading(false);
+        console.log("Fetched courses:", response.data);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+        setLoading(false);
+      }
+    };
+    fetchCourses();
+  }, []);
 
   const handleChangePage = (
     event: React.ChangeEvent<unknown>,
     value: number,
   ) => {
     setNowPage(value);
-    setCoursePage(coursePagesData.courseList[value - 1]);
+    setCoursePage(coursePagesData?.courseList[value - 1]);
   };
 
   useEffect(() => {
@@ -30,26 +58,34 @@ const MyCourse = () => {
   return (
     <div className={style.courseWrapper}>
       <div className={style.subTitle}>나의 코스</div>
-      <div>
-        {coursePage &&
-          coursePage.page.map((c, i) => (
-            <div
-              className={i === 0 ? undefined : style.courseList}
-              key={c.courseId}
-            >
-              <Course {...c} />
-            </div>
-          ))}
-      </div>
-      <div className={style.paginationWrapper}>
-        <Stack spacing={2}>
-          <Pagination
-            count={coursePagesData.courseList.length}
-            page={nowPage}
-            onChange={handleChangePage}
-          />
-        </Stack>
-      </div>
+      {coursePage ? (
+        <>
+          <div>
+            {coursePage &&
+              coursePage.page.map((c, i) => (
+                <div
+                  className={i === 0 ? undefined : style.courseList}
+                  key={c.courseId}
+                >
+                  <Course {...c} />
+                </div>
+              ))}
+          </div>
+          <div className={style.paginationWrapper}>
+            <Stack spacing={2}>
+              <Pagination
+                count={coursePagesData?.courseList.length}
+                page={nowPage}
+                onChange={handleChangePage}
+              />
+            </Stack>
+          </div>
+        </>
+      ) : (
+        <div style={{ margin: "30px 10px", color: "#aaa" }}>
+          내가 만든 코스가 아직 없어요. 😢
+        </div>
+      )}
     </div>
   );
 };
