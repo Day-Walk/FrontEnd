ad8b0f27fb27c5774fb1984bfff2a906fc932b9e
inthhh <glass005533@gmail.com>
2025-06-09 17:44:02 +0900

    feat: 코스 전체 조회 api 연동

--- src/courses_page/Courses.tsx
+++ src/courses_page/Courses.tsx
@@ -5,71 +5,107 @@ import * as Interfaces from "./interfaces/Interfaces";
 import SearchBox from "./components/SearchBox";
 import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
+import { useRecoilValue } from "recoil";
+import { userId } from "../recoil/userInfo";
+import { api } from "../utils/api";
 
 const Courses = () => {
   const [coursePagesData, setCoursePagesData] =
-    useState<Interfaces.CourseListResponse>(Interfaces.dummyCourseList);
+    useState<Interfaces.CourseListResponse>();
 
   const [loading, setLoading] = useState<boolean>(true);
   const [nowPage, setNowPage] = useState<number>(1);
   const [sort, setSort] = useState<string>("like"); // like or latest
-  const [coursePage, setCoursePage] = useState<Interfaces.CoursePage>(
-    coursePagesData.courseList[nowPage - 1],
-  );
+  const [coursePage, setCoursePage] = useState<Interfaces.CoursePage>();
+  // coursePagesData.courseList[nowPage - 1],
 
   const handleChangePage = (
     event: React.ChangeEvent<unknown>,
     value: number,
   ) => {
     setNowPage(value);
-    setCoursePage(coursePagesData.courseList[value - 1]);
+    setCoursePage(coursePagesData?.courseList[value - 1]);
   };
 
   useEffect(() => {
     console.log(nowPage, "coursePage", coursePage);
   }, [nowPage, coursePage]);
 
+  const userIdState = useRecoilValue(userId);
+  const token = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await api.get<Interfaces.CourseListResponse>(
+          `/course/all?sort=${sort}&userId=${userIdState}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        setCoursePagesData(response.data);
+        setCoursePage(response.data.courseList[nowPage - 1]);
+        setLoading(false);
+        console.log("Fetched courses:", response.data);
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+        setLoading(false);
+      }
+    };
+    fetchCourses();
+  }, []);
+
   return (
     <div className={style.courseWrapper}>
       {/* <div>코스 보기</div> */}
       <div>
         <SearchBox />
-        <div className={style.sortWrapper}>
-          <button
-            className={sort == "like" ? style.click : undefined}
-            onClick={() => setSort("like")}
-          >
-            인기순
-          </button>
-          <p>|</p>
-          <button
-            className={sort == "latest" ? style.click : undefined}
-            onClick={() => setSort("latest")}
-          >
-            최신순
-          </button>
-        </div>
       </div>
-      <div>
-        {coursePage &&
-          coursePage.page.map((c, i) => (
-            <div
-              className={i === 0 ? undefined : style.courseList}
-              key={c.courseId}
+      {coursePage ? (
+        <>
+          <div className={style.sortWrapper}>
+            <button
+              className={sort == "like" ? style.click : undefined}
+              onClick={() => setSort("like")}
             >
-              <Course {...c} />
-            </div>
-          ))}
-      </div>
-      <div className={style.paginationWrapper}>
-        <Stack spacing={2}>
-          <Pagination
-            count={coursePagesData.courseList.length}
-            page={nowPage}
-            onChange={handleChangePage}
-          />
-        </Stack>
-      </div>
+              인기순
+            </button>
+            <p>&nbsp;|&nbsp;</p>
+            <button
+              className={sort == "latest" ? style.click : undefined}
+              onClick={() => setSort("latest")}
+            >
+              최신순
+            </button>
+          </div>
+          <div>
+            {coursePage &&
+              coursePage.page.map((c, i) => (
+                <div
+                  className={i === 0 ? undefined : style.courseList}
+                  key={c.courseId}
+                >
+                  <Course {...c} />
+                </div>
+              ))}
+          </div>
+          <div className={style.paginationWrapper}>
+            <Stack spacing={2}>
+              <Pagination
+                count={coursePagesData.courseList.length}
+                page={nowPage}
+                onChange={handleChangePage}
+              />
+            </Stack>
+          </div>
+        </>
+      ) : (
+        <div style={{ margin: "60px 10px", color: "#aaa" }}>
+          해당하는 코스가 없어요. 😢
+        </div>
+      )}
     </div>
   );
 };
