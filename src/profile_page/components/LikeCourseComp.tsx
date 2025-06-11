21c6aa74a9d05d7fa541cf9d6056aa92b0875c0e
inthhh <glass005533@gmail.com>
2025-06-11 11:22:47 +0900

    feat: Profile - 코스 찜 취소 api 연동 및 코스 상세페이지 이동 추가

--- src/profile_page/components/LikeCourseComp.tsx
+++ src/profile_page/components/LikeCourseComp.tsx
@@ -4,10 +4,22 @@ import * as Interfaces from "../interfaces/Interfaces";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { MapPin } from "lucide-react";
 import NoImage from "../../assets/NoImage.png";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userId } from "../../recoil/userInfo";
+import { api } from "../../utils/api";
 
 const LikeCourseList = (nowCourse: Interfaces.Course) => {
   const [course, setCourse] = useState<Interfaces.Course | null>(nowCourse);
-  const [like, setLike] = useState<boolean>(course?.like || true);
+  const [like, setLike] = useState<boolean>(true);
+  const courseId = nowCourse.courseId;
+  const navigate = useNavigate();
+  const userIdState = useRecoilValue(userId);
+  const token = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    setLike(course?.like || true);
+  }, [course]);
 
   const LikeIcon = () => {
     return like ? (
@@ -17,22 +29,57 @@ const LikeCourseList = (nowCourse: Interfaces.Course) => {
     );
   };
 
+  const handleLike = async () => {
+    const body = {
+      userId: userIdState,
+      courseId: courseId,
+    };
+
+    try {
+      if (!like) {
+        // 좋아요 등록
+        await api.post("/course-like", body, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      } else {
+        // 좋아요 취소
+        await api.delete("/course-like", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          data: body,
+        });
+      }
+
+      setLike(!like);
+    } catch (error) {
+      console.error("좋아요 처리 실패:", error);
+      alert("좋아요 처리 중 오류가 발생했습니다.");
+    }
+  };
+
+  const handleCourseClick = () => {
+    navigate(`/course/${courseId}`);
+  };
+
   return (
     <div className={style.courseBlock}>
       <div className={style.header}>
-        <div className={style.title}>
+        <div onClick={handleCourseClick} className={style.title}>
           "{course?.userName}" 님의{" "}
           <span style={{ color: "var(--color-main)" }}>{course?.title}</span>{" "}
           코스
         </div>
         <div className={style.like}>
           <div>{course?.courseLike}&nbsp;</div>
-          <div onClick={() => setLike(!like)}>
+          <div onClick={handleLike}>
             <LikeIcon />
           </div>
         </div>
       </div>
-      <div className={style.coursePlaceList}>
+      <div onClick={handleCourseClick} className={style.coursePlaceList}>
         {course?.placeList.map((place, idx) => (
           <div key={place.placeId} className={style.placeBlock}>
             {place.imgUrl ? (
@@ -44,6 +91,7 @@ const LikeCourseList = (nowCourse: Interfaces.Course) => {
             ) : (
               <img src={NoImage} className={style.placeImg} />
             )}
+
             <div className={style.placeInfo}>
               <div className={style.idx}>{idx + 1}</div>
               {place.placeName.length < 9 ? (
@@ -51,6 +99,7 @@ const LikeCourseList = (nowCourse: Interfaces.Course) => {
               ) : (
                 <div className={style.placeNameEllipsis}>{place.placeName}</div>
               )}
+
               <div className={style.address}>
                 <MapPin size={14} />
                 {place.address.split(" ").slice(0, 2).join(" ")}
