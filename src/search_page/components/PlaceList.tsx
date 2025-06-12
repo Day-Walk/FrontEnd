bb9e9ef6765c80934544e63b31ba4abae8ee2619
inthhh <glass005533@gmail.com>
2025-06-12 11:15:29 +0900

    style: 검색결과 기본 이미지 설정 및 별점 반올림 제거

--- src/search_page/components/PlaceList.tsx
+++ src/search_page/components/PlaceList.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import style from "../Search.module.css";
 import { MapPin, Star, ThumbsUp } from "lucide-react";
 import * as Interfaces from "../interfaces/Interface";
-
+import NoImage from "../../assets/NoImage.png";
 interface Props {
   places: Interfaces.SearchPlace[];
   selectedPlaceId: string;
@@ -27,7 +27,11 @@ const RecommendedList: React.FC<Props> = ({
               : style.placeBlock
           }
         >
-          <img src={p.imgUrl} alt={p.placeName} className={style.placeImg} />
+          {p.imgUrl ? (
+            <img src={p.imgUrl} alt={p.placeName} className={style.placeImg} />
+          ) : (
+            <img src={NoImage} className={style.placeImg} />
+          )}
           <div className={style.placeInfo}>
             <div style={{ display: "flex", alignItems: "center" }}>
               <div className={style.placeName}>&nbsp;{p.placeName}</div>
@@ -37,12 +41,10 @@ const RecommendedList: React.FC<Props> = ({
               {p.address}
             </div>
             <div className={style.placeCategory}>
-              <div className={style.category}>
-                {p.category}({p.subCategory})
-              </div>
+              <div className={style.category}>{p.subCategory}</div>
               <div className={style.stars}>
                 <Star size={20} fill="#fabd55" color="#fabd55" />
-                <span>&nbsp;{p.stars.toFixed(1)}</span>
+                <span>&nbsp;{p.stars}</span>
               </div>
             </div>
           </div>
