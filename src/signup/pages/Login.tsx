74c7fbc0a950ce3cefef419105c4ee5474fc1fad
heedong12 <dlgmldus3289@naver.com>
2025-06-18 10:07:34 +0900

    fix: 배포환경 redirect_url 오류

--- src/signup/pages/Login.tsx
+++ src/signup/pages/Login.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../Login.module.css";
 import Logo from "../../assets/DayWalkLogo.png";
 
 const Login = () => {
   const handleClickLogin = () => {
     const REST_API_KEY = "e3325f522736f4e123141deaf49845ae";
-    const REDIRECT_URI = "http://localhost:5173/auth/kakao-callback";
+    const REDIRECT_URI = isProduction
+      ? "https://final-front-end-fawn.vercel.app/auth/kakao-callback"
+      : "http://localhost:5173/auth/kakao-callback";
+
     const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
     window.location.href = url;
   };
+
+  const [isProduction, setIsProduction] = React.useState<boolean>(false);
+  useEffect(() => {
+    if (import.meta.env.MODE === "production") {
+      setIsProduction(true);
+    }
+  }, []);
+  import.meta.env.MODE;
   return (
     <div className={styles.background}>
       <img src={Logo} className={styles.logo} />
