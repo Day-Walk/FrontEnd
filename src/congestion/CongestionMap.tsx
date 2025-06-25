98fdb0deb94cd5458d385796685632bdfc8617a3
heedong12 <dlgmldus3289@naver.com>
2025-06-25 22:03:12 +0900

    style: 혼잡도 스타일 수정

--- src/congestion/CongestionMap.tsx
+++ src/congestion/CongestionMap.tsx
@@ -42,7 +42,7 @@ const MARKS = [
 ];
 
 const CongestionMap = () => {
-  const [tabIndex, setTabIndex] = useState(0);
+  const [tabIndex, setTabIndex] = useState(1);
   const [loading, setLoading] = useState(false);
   const [markerValue, setMarkerValue] = useState<number>(1);
 
@@ -79,6 +79,13 @@ const CongestionMap = () => {
     }
   };
 
+  const getPredictedDateTime = () => {
+    const date = new Date();
+    date.setHours(date.getHours() + markerValue);
+
+    return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 ${date.getHours()}시`;
+  };
+
   useEffect(() => {
     getData();
   }, []);
@@ -98,41 +105,74 @@ const CongestionMap = () => {
         className={loading ? styles.loading : ""}
       >
         <div className={styles.wrapper}>
-          {["실시간 혼잡도", "혼잡도 예측"].map((label, index) => {
-            const ButtonComponent =
-              tabIndex === index ? MainButton : BorderButton;
-            return (
-              <ButtonComponent
-                key={index}
-                onClick={() => {
-                  setTabIndex(index);
-                }}
-                style={{ height: "46px", borderRadius: "20px", width: "100%" }}
-              >
-                {label}
-              </ButtonComponent>
-            );
-          })}
           <div className={styles.refresh_button} onClick={getData}>
             새로고침
             <RotateCcw size={16} color="#333" />
           </div>
+          <div
+            style={{
+              width: "100%",
+              display: "grid",
+              gap: "10px",
+              gridTemplateColumns: "repeat(2, 1fr)",
+            }}
+          >
+            {["실시간 혼잡도", "예측 혼잡도"].map((label, index) => {
+              const ButtonComponent =
+                tabIndex === index ? MainButton : BorderButton;
+              return (
+                <ButtonComponent
+                  key={index}
+                  onClick={() => {
+                    setTabIndex(index);
+                  }}
+                  style={{
+                    height: "46px",
+                    borderRadius: "20px",
+                    width: "100%",
+                    flex: 1,
+                  }}
+                  fontColor={tabIndex !== index ? "#888" : "#fff"}
+                >
+                  {label}
+                </ButtonComponent>
+              );
+            })}
+          </div>
           {tabIndex === 1 && (
-            <Slider
-              marks={MARKS}
-              defaultValue={1}
-              step={null}
-              valueLabelDisplay="auto"
-              max={12}
-              min={1}
-              style={{ width: "320px" }}
-              value={markerValue}
-              onChange={(e, value) => {
-                setMarkerValue(value as number);
-              }}
-            ></Slider>
+            <>
+              <div style={{ width: "100%" }}>
+                <div style={{ fontSize: "14px", color: "#888" }}>시간</div>
+                <div style={{ width: "100%", padding: "0 10px" }}>
+                  <Slider
+                    marks={MARKS}
+                    defaultValue={1}
+                    step={null}
+                    valueLabelDisplay="auto"
+                    max={12}
+                    min={1}
+                    style={{ width: "100%", color: "#00b493" }}
+                    value={markerValue}
+                    onChange={(e, value) => {
+                      setMarkerValue(value as number);
+                    }}
+                  ></Slider>
+                </div>
+              </div>
+              <div style={{ width: "100%", lineHeight: "1.5" }}>
+                예측 시간은 {new Date().getHours()}시 기준입니다.
+                <br />
+                <br /> 현재&nbsp;
+                <span style={{ fontWeight: "bold", color: "#E96563" }}>
+                  {getPredictedDateTime()}
+                </span>
+                의 <br />
+                예측 혼잡도를 보여주고 있습니다.
+              </div>
+            </>
           )}
           <hr color="#e5e5e5" style={{ width: "100%" }} />
+          <div style={{ width: "100%" }}>마커 색상 안내</div>
           <div className={styles.marker_wrapper}>
             <Marker color="#EF4444" size={40} shadow={true} />
             <Marker color="#F97316" size={40} shadow={true} />
