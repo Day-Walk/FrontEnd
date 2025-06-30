92ac7cc62cda99820eb7da5fe855470f757fac65
heedong12 <dlgmldus3289@naver.com>
2025-06-30 10:47:10 +0900

    feat: zustand 설치 및 안쓰는 라이브러리 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")!).render(
-  <RecoilRoot>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </RecoilRoot>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
